2586d3a0dfc1e436d64c8c65ad830fe76a5cb3d5
wkd <2053617137@qq.com>
2022-09-14 20:40:24 +0800

    init

---
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.weather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        每次随机一个天气显示：阴晴雨雪
+//        Weather class 天气城市温度
+
+    }
+}
